173158d79817bc51ca7ec6ed780f56c30ecdd237
winstxnhdw@gmail.com <winstxnhdw@gmail.com>
2021-03-27 09:49:46 +0800

    removed useless modules

--- animate.py
+++ animate.py
@@ -1,7 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import os
 
 from kinematic_model import KinematicBicycleModel
 from matplotlib.animation import FuncAnimation
