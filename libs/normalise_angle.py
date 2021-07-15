688fa6b73cf769149b4da557d1860517ced51432
Winston <56998716+winstxnhdw@users.noreply.github.com>
2021-07-15 21:14:45 +0800

    Update normalise_angle.py

--- libs/normalise_angle.py
+++ libs/normalise_angle.py
@@ -1,11 +1,7 @@
 import math as m
 
 def normalise_angle(angle):
-    """
-    Normalize an angle to [-pi, pi].
-    :param angle: (float)
-    :return: (float) Angle in radian in [-pi, pi]
-    """
+
     angle = m.atan2(m.sin(angle), m.cos(angle))
 
     return angle
