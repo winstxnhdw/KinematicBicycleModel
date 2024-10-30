459b769f6a1aba65b01cd0da523519932f4db021
winstxnhdw <winstxnhdw@gmail.com>
2024-10-30 01:21:58 +0000

    build: ship type stubs

--- kbm/exceptions.py
+++ kbm/exceptions.py
@@ -1,3 +1,3 @@
-class NegativeValueError(Exception):
+class NegativeValueError(ValueError):
     def __init__(self, argument: str) -> None:
         super().__init__(f"{argument} must be positive.")
