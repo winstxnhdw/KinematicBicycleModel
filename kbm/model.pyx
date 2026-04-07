e453f2fda915d769cd4064e088298bbd82855327
winstxnhdw <winstxnhdw@gmail.com>
2026-04-08 01:46:40 +0800

    perf: use `remainder` instead of `atan2`

--- kbm/model.pyx
+++ kbm/model.pyx
@@ -1,7 +1,8 @@
 # cython: language_level=3, cdivision=True, auto_pickle=False, binding=False
 
-from libc.math cimport atan2, cos, sin, tan
+from libc.math cimport atan2, cos, fmax, fmin, sin, tan, remainder, M_PI
 from kbm.exceptions import NegativeValueError
+
 
 cdef packed struct VehicleState:
     double x
@@ -10,6 +11,22 @@ cdef packed struct VehicleState:
     double steer
     double velocity
     double angular_velocity
+
+
+cdef inline double clip(const double value, const double min_value, const double max_value) noexcept nogil:
+    return fmin(max_value, fmax(min_value, value))
+
+
+cdef inline double normalise_angle(const double angle) noexcept nogil:
+    return remainder(angle, 2 * M_PI)
+
+
+cdef inline double compute_angular_velocity(
+    const double velocity,
+    const double steer,
+    const double wheelbase
+) noexcept nogil:
+    return velocity * tan(steer) / wheelbase
 
 
 cdef inline const VehicleState compute_vehicle_state(
@@ -25,17 +42,15 @@ cdef inline const VehicleState compute_vehicle_state(
 ) noexcept nogil:
     cdef VehicleState state
 
+    new_steer = clip(steer, -max_steer, max_steer)
     new_velocity = velocity + delta_time * acceleration
-    new_steer = max_steer if steer > max_steer else -max_steer if steer < -max_steer else steer
-    angular_velocity = new_velocity * tan(new_steer) / wheelbase
-    new_yaw = yaw + angular_velocity * delta_time
     velocity_delta = new_velocity * delta_time
-    cos_new_yaw = cos(new_yaw)
-    sin_new_yaw = sin(new_yaw)
+    angular_velocity = compute_angular_velocity(new_velocity, new_steer, wheelbase)
+    new_yaw = yaw + angular_velocity * delta_time
 
-    state.x = x + velocity_delta * cos_new_yaw
-    state.y = y + velocity_delta * sin_new_yaw
-    state.yaw = atan2(sin_new_yaw, cos_new_yaw)
+    state.x = x + velocity_delta * cos(new_yaw)
+    state.y = y + velocity_delta * sin(new_yaw)
+    state.yaw = normalise_angle(new_yaw)
     state.steer = new_steer
     state.velocity = new_velocity
     state.angular_velocity = angular_velocity
